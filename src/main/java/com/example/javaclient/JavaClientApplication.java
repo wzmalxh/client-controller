60901529679f206c49b8a83f8c92ce74f358560e
wzmeng <278582065@qq.com>
2024-11-17 14:24:04 +0800

    add: 添加一个根项目仓库

---
+++ src/main/java/com/example/javaclient/JavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaClientApplication.class, args);
+    }
+
+}
